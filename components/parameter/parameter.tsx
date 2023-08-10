3c74529ef8ea08381a1e266b307431d05ce311fe
Tom Elliott <tom@letsdothis.com>
2023-08-10 12:41:33 +0100

    Linting changes

--- components/parameter/parameter.tsx
+++ components/parameter/parameter.tsx
@@ -1,25 +1,29 @@
-import styles from './parameter.module.css'
+import styles from "./parameter.module.css";
 
 export type ParameterProps = {
-  name: string,
-  type: string,
-  required: boolean, 
-  children: string,
-}
+  name: string;
+  type: string;
+  required: boolean;
+  children: string;
+};
 
-export const Parameter = ({ name, type, required, children }: ParameterProps) => {
-
+export const Parameter = ({
+  name,
+  type,
+  required,
+  children,
+}: ParameterProps) => {
   return (
     <div className={styles.container}>
       <hr className={styles.splitter} />
       <div>
         <code className={styles.name}>{name}</code>
         <span className={styles.type}>{type}</span>
-        { required && <span className={styles.required}>Required</span> }
+        {required && <span className={styles.required}>Required</span>}
       </div>
       <div className={styles.descriptionContainer}>
         <span>{children}</span>
       </div>
     </div>
-  )
-}
+  );
+};
